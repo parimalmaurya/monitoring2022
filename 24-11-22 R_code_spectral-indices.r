bda445f1f48e04d9cfd65a8bf81a44d338be7cf6
parimalmaurya <116761863+parimalmaurya@users.noreply.github.com>
2022-11-24 22:19:09 +0530

    Create 24-11-22 R_code_spectral-indices.r

---
+++ 24-11-22 R_code_spectral-indices.r
@@ -0,0 +1,6 @@
+# calculating spectral?vegetation indices from remote sensing
+# threshold for tress
+library (RStoolbox) # for classification 
+# unsupervised classification ( we're not stating anythging to the software, the software is doinbg this on its own)
+
+> un
