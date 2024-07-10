3b4332e9cbd2ffcbff9cd58970ec278cdadaecb4
parimalmaurya <116761863+parimalmaurya@users.noreply.github.com>
2024-07-10 15:31:01 +0530

    Delete 24-11-22 R_code_spectral-indices.r

--- 24-11-22 R_code_spectral-indices.r
+++
@@ -1,6 +0,0 @@
-# calculating spectral?vegetation indices from remote sensing
-# threshold for tress
-library (RStoolbox) # for classification 
-# unsupervised classification ( we're not stating anythging to the software, the software is doinbg this on its own)
-
-> un
